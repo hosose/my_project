6612f683ebe73bcd0233f9482c24788febfddb6b
hosose <hosose22@gmail.com>
2026-07-22 14:29:11 +0900

    Feat: 홈페이지 1차 개발 완료

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # 간단한 홈페이지 제공
 # 1. module 가져오기
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 # 2. Fastapi 객체 생성, 전역변수 생성
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 # 3. 라우팅 구성/정의
@@ -19,3 +22,38 @@ def home(req:Request):
         request=req, 
         name="index.html"
     )
+
+# 2. index.html 직접 요청
+@app.get("/index.html")
+def index_page(req: Request):
+    return templates.TemplateResponse(
+        request=req, 
+        name="index.html"
+    )
+
+# 3. index2.html 요청
+@app.get("/index2.html")
+def index2_page(req: Request):
+    return templates.TemplateResponse(
+        request=req, 
+        name="index2.html"
+    )
+
+# 4. index3.html 요청
+@app.get("/index3.html")
+def index3_page(req: Request):
+    return templates.TemplateResponse(
+        request=req, 
+        name="index3.html"
+    )
+
+'''
+# 동적으로도 가능
+@app.get("/{page_name}.html")
+def render_page(page_name: str, req: Request):
+    # /index2.html 요청 시 templates/index2.html 렌더링
+    return templates.TemplateResponse(
+        request=req,
+        name=f"{page_name}.html"
+    )
+'''
